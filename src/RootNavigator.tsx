aad601201c0d5227f59052dbe816879971112120
Gustavo Barros <gustavoasb@gmail.com>
2021-07-16 08:09:34 -0300

    ✨ Setup initial navigator

---
+++ src/RootNavigator.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// import Login from "./pages/Login";
+// import Home from "./pages/Home";
+
+const Stack = createStackNavigator();
+
+import { useUser } from "./context/UserContext";
+import OnboardingFirst from "./pages/Onboarding/OnboardingFirst";
+import OnboardingSecond from "./pages/Onboarding/OnboardingSecond";
+import Feed from "./pages/Feed";
+
+export default function RootNavigator() {
+  const { user } = useUser();
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {user ? (
+        <Stack.Screen name="Home" component={Feed} />
+      ) : (
+        <>
+          <Stack.Screen name="OnboardingFirst" component={OnboardingFirst} />
+          <Stack.Screen name="OnboardingSecond" component={OnboardingSecond} />
+        </>
+      )}
+    </Stack.Navigator>
+  );
+}
